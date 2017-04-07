577376dd65969a19de92973730f4cae5228a5f40
Paul Martin <Dmart331@gmail.com>
2017-04-07 09:45:45 -0500

    Added function to make movie get

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../shared.service';
 
 @Component({
   selector: 'app-movie',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class MovieComponent implements OnInit {
+	id_movie: string = "";
+	mv_Title: string = "";
+	mv_Rated: string = "";
+	mv_Released: string = "";
+	mv_Director: string = "";
+	mv_Actors: string = "";
+	mv_Plot: string = "";
 
-  constructor() { }
+  constructor(private _sharedService: SharedService ) { }
 
   ngOnInit() {
   }
 
+  callMovieService(){
+  	this._sharedService.findMovie(this.id_movie)
+  		.subscribe( listResult => {
+  			        this.mv_Title = listResult["Title"];
+                this.mv_Rated = listResult["Rated"];
+ 
+                this.mv_Released = listResult["Released"];
+                this.mv_Director = listResult["Director"];
+                this.mv_Actors = listResult["Actors"];
+                this.mv_Plot = listResult["Plot"];
+  		},
+  		error => {
+  			console.log("Error");
+  			console.log("See, told ya");
+  		}
+  		);
+  }
 }
