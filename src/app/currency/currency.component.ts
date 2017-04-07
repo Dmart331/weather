36e660e73d710e22892044f22a33367c11163d2f
Paul Martin <Dmart331@gmail.com>
2017-04-07 09:49:36 -0500

    Added currency getter

--- src/app/currency/currency.component.ts
+++ src/app/currency/currency.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../shared.service';
 
 @Component({
   selector: 'app-currency',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrencyComponent implements OnInit {
 
-  constructor() { }
+	id_currency: string = "";
+	my_result: any;
+
+  constructor(private _sharedService : SharedService) { }
 
   ngOnInit() {
   }
 
+  callCurrencyService(){
+  	this._sharedService.findMovie(this.id_currency)
+  		.subscribe( listResult => {
+  			        this.my_result =  JSON.stringify(listResult);
+  		},
+  		error => {
+  			console.log("Error");
+  			console.log("See, told ya");
+  		}
+  		);
+  }
 }
